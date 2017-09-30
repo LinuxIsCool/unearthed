9bdcbe28d1f1aaeeeea785b28cdc46aef1c07efa
Shawn Anderson <swanders@ualberta.ca>
2017-09-30 15:01:49 -0700

    template changes

--- frothgui/views.py
+++ frothgui/views.py
@@ -11,7 +11,32 @@ def current_datetime(request):
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
 
+def get_data():
+    data = {
+            'air_addition_rate':{
+                'current': 1,
+                'optimal': 1.1,
+                },
+            'frother_addition_rate':{
+                'current': 12,
+                'optimal': 11,
+                },
+            'slurry_density':{
+                'current': 1,
+                'optimal': 1.5,
+                },
+            'activator_addition_rate':{
+                'current': 0.6,
+                'optimal': 1.2,
+                },
+            'collector_addition_rate':{
+                'current': 3,
+                'optimal': 1.1,
+                },
+            }
+    return data
+
 def show_data(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return render(request, 'frothgui/data.html', {'date':now})
+    data = get_data()
+    return render(request, 'frothgui/data.html', {'data':data})
